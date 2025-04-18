3f59676cffaf79f7ea3888d6ac74822f4b7d02cf
Demas20 <demasabiansya@gmail.com>
2025-04-18 16:41:28 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // Pastikan ini mencakup file HTML yang sudah dibuat
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
